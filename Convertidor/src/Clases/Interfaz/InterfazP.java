c0afe70bea4c0c29c330dd3793a4b5c869e68fe1
Rudy Daniel <rudydanielcarballo@gmail.com>
2025-04-04 21:04:56 +0200

    Cambiamos la direccion de la imagen

--- Convertidor/src/Clases/Interfaz/InterfazP.java
+++ Convertidor/src/Clases/Interfaz/InterfazP.java
@@ -8,7 +8,7 @@ import java.awt.*;
 
 public class InterfazP extends JFrame {
     
-    private static final String FONDO_IMAGEN_PATH = "Convertidor\\Convertidor\\src\\Resources\\Fondo.jfif";
+    private static final String FONDO_IMAGEN_PATH = "Convertidor\\src\\Resources\\Fondo.jfif";
     private JButton boton1, boton2, boton3, botonMenu;
     private JPanel panelBotones, panelBotonesInstrucciones;
     private JScrollPane scrollPaneInstrucciones;
